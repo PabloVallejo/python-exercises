f0a99e478bd11363856389d9a41a8ba094273863
Pablo Vallejo <pabloninety@gmail.com>
2013-06-28 08:00:19 -0500

    Still working on split_setences(), looking for regexps to do the job.

--- sections/module_4.py
+++ sections/module_4.py
@@ -38,15 +38,21 @@ def split_sentences( filename = 'data/text-42.md' ):
 
     # 1. Periods followed by whitespace followed by a lower case letter are not sentence boundaries.
     # i.e. sample
+    # Sol: Add new line in Dot followed by space Followed by Uppercase Letter
+    # re -> '\.\s[A-Z]'
+    # re ->
 
     # 2. Periods followed by a digit with no intervening whitespace are not sentence boundaries.
     # 1.5
+    # Find -> '\.\d+'
 
     # 3. Periods followed by whitespace and then an upper case letter, but preceded by any of a short list of titles are not sentence boundaries. Sample titles include Mr., Mrs., Dr., and so on.
     # Mr. John, Mrs. John, Dr. John ...
+    # Find -> '(Mr.|Mrs.|Dr.)\s[A-Z]'
 
     # 4. Periods internal to a sequence of letters with no adjacent whitespace are not sentence boundaries (for example, www.aptex.com, or e.g).
     # www.sample.com, e.g ..
+    #
 
     # 5. Periods followed by certain kinds of punctuation (notably comma and more periods) are probably not sentence boundaries.
     # ...
